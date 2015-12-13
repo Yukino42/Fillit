4f6171251b40dfee7eab737016bc76d641829a35
Cyrus42 <gautier.hubault@gmail.com>
2015-12-13 17:02:36 +0100

    push 3

--- main.c
+++ main.c
@@ -29,76 +29,104 @@ void  ft_error_exit(char *str)
 	  i++;
   }
   write(1, "\n", 1);
-  exit;
+  exit(0);
 }
 
-int		ft_check_diez(char **tab, int Y, int X)
+int		ft_check_diez_block(char **tab, int Y, int X)
 {
 	int i;
 	int y;
 	int x;
-	
+	int line;
+
 	x = X;
 	y = Y;
 	i = 0;
-	if (tab[y+1][X] %5 == 0)
-	{
-		if((tab[Y][x-1]) == '#')
-			i++;
-		if((tab[Y][x+2]) == '#')
-			i++;
-		if((tab[Y+2][x-1]) == '#')
-			i++;
-	}
+	line = 0;
+	if (((y-1) %4) == 0)
+		line++;
+	else if ((tab[y][X]) == '#')
+		i++;
+	if((tab[Y][x-1]) == '#')
+		i++;
+	if((tab[Y][x+2]) == '#')
+		i++;
+	if((tab[y+2][X]) == '#')
+		i++;
 	if (i == 0)
 		return (0);
-	else if (i != 0)
-		return (1);
-	return (2);
+	return (1);
 }
+
+void	ft_print_tetri(char **tab)
+{
+	int x;
+	int y;
+	char c;
+
+	x = 0;
+	y = 0;	
+	while(tab[y])
+	{
+		while(tab[y][x])
+			{
+				c = tab[y][x];
+				write(1, &c, 1);
+				x++;
+			}
+			write(1, "\n", 1);
+			y++;
+			x = 0;			
+	}
+	exit(0);
+}
+
+
 
 void    ft_error(char *argv)
 {
-  char	buff[BUFF_SIZE + 1];
-  char	**tab;
-  int   file_descriptor;
-  int	read_result;
-  int	tetri_nomber;
-  int	counter;
-  int   x;
-  int   y;
-  int   k;
-  
-  if ((file_descriptor = open(argv, O_RDONLY)) == -1)
-    	ft_error_exit("error");
+	char	buff[BUFF_SIZE + 1];
+	char	**tab;
+	int   	file_descriptor;
+	int		read_result;
+	int		tetri_nomber;
+	int		counter;
+	int   	x;
+	int   	y;
+	int   	k;
+	int 	fake;
+
+	if ((file_descriptor = open(argv, O_RDONLY)) == -1)
+		ft_error_exit("error1");
 	if ((read_result = read(file_descriptor, buff, BUFF_SIZE)) == -1)
-  	ft_error_exit("error");
-  buff[read_result] = '\0';
-if (close(file_descriptor) == -1)
-	ft_error_exit("error");
-if (((read_result + 1) % 21) != 0)
-	ft_error_exit("error");
-if ((tab = ft_strsplit(buff, '\n')) == NULL)
-	ft_error_exit("error");
+  		ft_error_exit("error2");
+	buff[read_result] = '\0';
+	if (close(file_descriptor) == -1)
+		ft_error_exit("error3");
+	if (((read_result + 1) % 21) != 0)
+		ft_error_exit("error4");
+	if ((tab = ft_strsplit(buff, '\n')) == NULL)
+		ft_error_exit("error5");
 	y = -1;
-  while (tab[++y])
+	while (tab[++y])
 	{
 		x = -1;
 		while (tab[y][++x])
 		{
-			if (tab[y][x] != '#' && tab[y][x] != '.')
-				ft_error_exit("error");
+			if (tab[y][x] != '#' && tab[y][x] != '.')\
+				ft_error_exit("error6");
 		}
 		if (x != 4)
-			ft_error_exit("error");
+			ft_error_exit("error7");
 	}
 	if ((y % 4) != 0)
-		ft_error_exit("error");
+		ft_error_exit("error8");
 	if ((tetri_nomber = (y / 4)) > 26)
-		ft_error_exit("error");
-  x = -1;
-  y = 0;
+		ft_error_exit("error9");
+	x = -1;
+	y = 0;
 	counter = 0;
+	fake = 0;
 	while (tab[y])
 	{
 		k = -1;
@@ -106,8 +134,13 @@ if ((tab = ft_strsplit(buff, '\n')) == NULL)
 		{
 			if (tab[y][++x] == '#')
 			{
-				if ((ft_check_diez(tab, y, x)) == 0)
-					ft_error_exit("error");
+				fake++;
+				if ((ft_check_diez_block(tab, y, x)) == 0)
+				{
+					ft_putnbr(fake);
+					write(1, " ", 1);
+					ft_error_exit("error10");
+				}	
 				counter++;
 			}
 				counter++;
@@ -118,8 +151,9 @@ if ((tab = ft_strsplit(buff, '\n')) == NULL)
 			}
 		}
 		if ((counter % 4) != 0)
-			ft_error_exit("error");
+			ft_error_exit("error11");
 	}
+	ft_print_tetri(tab);
 }
 
 int     main(int argc, char **argv)
