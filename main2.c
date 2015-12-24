60be9785af36326fbeac1434b1601cb293c65b14
Cyrus42 <gautier.hubault@gmail.com>
2015-12-24 19:30:00 +0100

    push de noel

--- main2.c
+++ main2.c
@@ -60,32 +60,77 @@ int		ft_count_sharp(char *buff)
 	return (1);
 }
 
-int		ft_count_char(char *buff)
+int		ft_count_char_sharp_line(char *buff)
+{
+	int i;
+	int j;
+	int new;
+	int sharp;
+	
+	i = 0;
+	j = 0;
+	new = 0;
+	sharp = 0;
+	while (buff[i])
+	{
+		while (buff[i] != '\n')
+		{
+			if (buff[i] == '#')
+				sharp++;
+			i++;
+			j++;
+		}
+		new++;
+		if (j != 4 && (new %5) != 0 && sharp %4 != 0)
+			return (0);			
+		j = 0;
+		i++;
+	}
+	return (1);
+}
+int		ft_count_back(char *buff)
+{
+	int i;
+	int j;
+	
+	i = 0;
+	j = 0;
+	while(buff[i])
+	{
+		if (buff[i] == '#')
+			j++;
+		i++;
+	}
+	return (j);
+}
+int		ft_check_block(char *buff)
 {
 	int i;
 	int j;
 	int new;
 	
 	i = 0;
-	j = 0;
-	new = 0;
 	while (buff[i])
 	{
-		while (new < 5)
+		j = 0;
+		new = 0;
+		while (new <= 4 && buff[i])
 		{
-			while (buff[i] != '\n')
-			{
+			if (buff[i] == '\n')
+				new++;
+			if ((i > 5) && buff[i - 5] && buff[i - 5] == '#' && buff[i] == '#')
 				j++;
-				i++;
-			}
-			new++;
+			if (buff[i - 1] && buff[i - 1] == '#' && buff[i] == '#')
+				j++;
+			if (buff[i + 1] && buff[i + 1] == '#' && buff[i] == '#')
+				j++;
+			if (buff[i + 5] && buff[i + 5] == '#' && buff[i] == '#'
+					&& (ft_count_back(buff) - 1) != new )
+				j++;
 			i++;
 		}
-		if (j != 4)
+		if (j < 6)
 			return (0);
-		new = 0;
-		j = 0;
-		i++;
 	}
 	return (1);
 }
@@ -97,11 +142,6 @@ void    ft_error(char *argv)
 	int   	file_descriptor;
 	int		read_result;
 	int		tetri_nomber;
-	int		counter;
-	int   	x;
-	int   	y;
-	int   	k;
-	int 	sharp;
 
 	if ((file_descriptor = open(argv, O_RDONLY)) == -1)
 		ft_error_exit("error1");
@@ -114,9 +154,12 @@ void    ft_error(char *argv)
 		ft_error_exit("error4");
 	if (ft_count_sharp(buff) == 0)
 		ft_error_exit("error5");
-	if (ft_count_char(buff) == 0)
+	if (ft_count_char_sharp_line(buff) == 0)
 		ft_error_exit("error5");
-	
+	if (ft_check_block(buff) == 0)
+		ft_error_exit("error6");
+		
+		
 	ft_print_buff(buff);
 }
 
